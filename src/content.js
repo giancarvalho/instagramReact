6a5a46c37e2e91ff7cd99024feb8c133d0c655f4
giancarvalho <giandcarvalho@gmail.com>
2021-08-21 14:43:39 -0300

    Broke down nav function

--- src/content.js
+++ src/content.js
@@ -1,11 +1,13 @@
 import Feed from "./feed";
 import Sidemenu from "./sidemenu";
+import MobileButtons from "./mobilebuttons";
 
 export default function Content() {
   return (
     <div class="corpo">
       <Feed />
       <Sidemenu />
+      <MobileButtons />
     </div>
   );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -1,35 +1,62 @@
+function Logo() {
+  return (
+    <div class="logo">
+      <ion-icon name="logo-instagram"></ion-icon>
+      <div class="separador"></div>
+      <img src="assets/img/logo.png" />
+    </div>
+  );
+}
+
+function Icons() {
+  return (
+    <div class="icones">
+      <ion-icon name="paper-plane-outline"></ion-icon>
+      <ion-icon name="compass-outline"></ion-icon>
+      <ion-icon name="heart-outline"></ion-icon>
+      <ion-icon name="person-outline"></ion-icon>
+    </div>
+  );
+}
+
+function LogoMobile() {
+  return (
+    <div class="logo-mobile">
+      <ion-icon name="logo-instagram"></ion-icon>
+    </div>
+  );
+}
+
+function InstagramMobile() {
+  return (
+    <div class="instagram-mobile">
+      <img src="assets/img/logo.png" />
+    </div>
+  );
+}
+
+function IconsMobile() {
+  return (
+    <div class="icones-mobile">
+      <ion-icon name="paper-plane-outline"></ion-icon>
+    </div>
+  );
+}
+
 export default function Navbar() {
   return (
     <div class="navbar">
       <div class="container">
-        <div class="logo">
-          <ion-icon name="logo-instagram"></ion-icon>
-          <div class="separador"></div>
-          <img src="assets/img/logo.png" />
-        </div>
-
-        <div class="logo-mobile">
-          <ion-icon name="logo-instagram"></ion-icon>
-        </div>
-
-        <div class="instagram-mobile">
-          <img src="assets/img/logo.png" />
-        </div>
+        <Logo />
+        <LogoMobile />
+        <InstagramMobile />
 
         <div class="pesquisa">
           <input type="text" placeholder="Pesquisar" />
         </div>
 
-        <div class="icones">
-          <ion-icon name="paper-plane-outline"></ion-icon>
-          <ion-icon name="compass-outline"></ion-icon>
-          <ion-icon name="heart-outline"></ion-icon>
-          <ion-icon name="person-outline"></ion-icon>
-        </div>
-
-        <div class="icones-mobile">
-          <ion-icon name="paper-plane-outline"></ion-icon>
-        </div>
+        <Icons />
+        <IconsMobile />
       </div>
     </div>
   );
